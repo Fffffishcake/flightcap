09bebd7916ff86398370ee3c96c937ee7fc19225
Fffffishcake <63023363+Fffffishcake@users.noreply.github.com>
2022-09-01 18:50:27 -0700

    calculate dates dynamically

--- static/script.js
+++ static/script.js
@@ -1,41 +1,19 @@
 'use strict';
 (async () => {
-    const dates = [
-        '2022-07-13',
-        '2022-07-15',
-        // '2022-07-16',
-        '2022-07-17',
-        '2022-07-20',
-        '2022-07-22',
-        '2022-07-23',
-        '2022-07-24',
-        '2022-07-27',
-        '2022-07-29',
-        '2022-07-30',
-        '2022-07-31',
-        '2022-08-03',
-        '2022-08-05',
-        '2022-08-06',
-        '2022-08-07',
-        '2022-08-10',
-        '2022-08-12',
-        '2022-08-13',
-        '2022-08-14',
-        '2022-08-17',
-        '2022-08-19',
-        '2022-08-20',
-        '2022-08-21',
-        '2022-08-24',
-        '2022-08-26',
-        '2022-08-27',
-        '2022-08-28',
-        '2022-08-31',
-    ];
+    const dates = [];
+    const startDate = new Date();
+    let date = new Date();
+    while ((date - startDate) / 1000 / 60 / 60 / 24 < 59) {
+        if ([0, 3, 5, 6].includes(date.getDay())) { // sun, wed, fri, sat
+            const monthStr = String(date.getMonth() + 1).padStart(2, '0');
+            const dateStr = String(date.getDate()).padStart(2, '0');
+            dates.push(`${date.getFullYear()}-${monthStr}-${dateStr}`);
+        }
+        date.setDate(date.getDate() + 1);
+    }
 
     const main = document.querySelector('#dates');
-    const now = new Date().toISOString();
     for (const date of dates) {
-        if (date < now) continue;
         const section = document.createElement('section');
         main.appendChild(section);
         loadCapacity(date, section);
